f6d4f28dd12f962d029b66e1f99ffe4beb9dce84
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2018-12-05 17:47:18 -0500

    Remove service worker import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
