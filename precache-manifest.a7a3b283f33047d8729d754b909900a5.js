25bd7a509b1e6adec443643d1ccfdb7d2373556c
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2018-12-08 10:40:43 -0500

    Updates

---
+++ precache-manifest.a7a3b283f33047d8729d754b909900a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d46ba5ab6750e6d35801",
+    "url": "/static/js/main.d46ba5ab.chunk.js"
+  },
+  {
+    "revision": "9f8512d8a528d8ff524b",
+    "url": "/static/js/1.9f8512d8.chunk.js"
+  },
+  {
+    "revision": "d46ba5ab6750e6d35801",
+    "url": "/static/css/main.783412f8.chunk.css"
+  },
+  {
+    "revision": "511bfb89d24859d0486e90c370567f08",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.465438ac2cef0f4a5379d2df4bd00ea4.js"
+  "/precache-manifest.a7a3b283f33047d8729d754b909900a5.js"
 );
 
 workbox.clientsClaim();
